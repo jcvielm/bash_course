3496f35e04d28ee26e37401441fb6db3ddd0b4cc
jcvielm <jcvielm@gmail.com>
2023-12-15 16:36:52 -0300

    opciones y descargar datos de internet

---
+++ 10_downloads.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+# Programa para ejemplificar el uso de la descarga de informacion desde internet utilizando el comando wget
+
+echo "Descargar informacion de internet"
+wget https://downloads.apache.org/tomcat/tomcat-8/v8.5.97/bin/apache-tomcat-8.5.97.zip
---
+++ 9_options.sh
@@ -0,0 +1,23 @@
+# !/bin/bash
+# Programa para ejemplificar como se realiza el paso de opciones con o sin parametro
+# Autor: Jose Vielman @jcvielm
+
+echo "Programa Opciones"
+echo "Opcion 1 enviada: $1"
+echo "Opcion 2 enviada: $2"
+echo "Opciones enviadas: $*"
+echo -e "\n"
+echo "Recuperar valores"
+while [ -n "$1" ]
+do
+case "$1" in
+-a) echo "-a opcion utilizada";;
+-b) echo "-b opcion utilizada";;
+-c) echo "-c opcion utilizada";;
+*) echo "$! no es una opcion";;
+esac
+shift
+done
+
+echo hola que tal $1
+
